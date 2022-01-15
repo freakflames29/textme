8c73f2ec6e9c3424cdd3174a762918552ca8a474
Sourav Das <iamsouravdaswade420@gmail.com>
2022-01-15 22:07:28 +0530

    added message to view and message partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :messages
+
   before_save :downcase_name_email
   has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
