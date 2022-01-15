226d41fbbafc921b36ebd175f83541940781d027
Sourav Das <iamsouravdaswade420@gmail.com>
2022-01-15 21:27:56 +0530

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  before_save :downcase_name_email
+  has_secure_password
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
+
+  EMAIL_REGEX = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX, multiline: true }
+
+  private
+
+  def downcase_name_email
+    self.username = username.downcase
+    self.email = email.downcase
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "sourav", email: "sd@das.com", password: "password", password_confirmation: "password")
+User.create(username: "xman", email: "xmand@das.com", password: "password", password_confirmation: "password")
+User.create(username: "deadpool", email: "deadpool@das.com", password: "password", password_confirmation: "password")
