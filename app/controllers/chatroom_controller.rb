2743457855981436249f8ee07e7da8ff8f657b90
Sourav Das <iamsouravdaswade420@gmail.com>
2022-01-16 14:07:08 +0530

    added require login in controller level

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,14 +1,9 @@
 class ChatroomController < ApplicationController
-  before_action :is_logged_in?
+  before_action :require_user
 
   def index
     @messages = Message.all
   end
 
-  private
-
-  def is_logged_in?
-    redirect_to login_path unless logged_in?
-  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+  before_action :if_logged_in?, except: [:destroy]
+
   def new
   end
 
   def create
-    
+
     @user = User.find_by(username: params[:session][:username].downcase)
     if @user&.authenticate(params[:session][:password])
       session[:user_id] = @user.id
@@ -17,9 +19,16 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id]=nil
+    session[:user_id] = nil
     flash[:success] = 'You are logged out'
     redirect_to login_path
   end
 
+  private
+
+  def if_logged_in?
+    if logged_in?
+      redirect_to root_path
+    end
+  end
 end
