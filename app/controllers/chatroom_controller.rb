eb034a64fd5c876ff16891469ca97e90ce77cc7c
Sourav Das <iamsouravdaswade420@gmail.com>
2022-01-16 12:30:58 +0530

    added login function

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,14 @@
 class ChatroomController < ApplicationController
+  before_action :is_logged_in?
+
   def index
     @messages = Message.all
+  end
+
+  private
+
+  def is_logged_in?
+    redirect_to login_path unless logged_in?
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render plain: params[:session]
+    
+    @user = User.find_by(username: params[:session][:username].downcase)
+    if @user&.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      puts "-----------------------++++--------------+++++------++++--#{logged_in?}"
+      flash[:success] = 'You are successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Hmm! You are entering wrong credentials'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id]=nil
+    flash[:success] = 'You are logged out'
+    redirect_to login_path
   end
 
 end
